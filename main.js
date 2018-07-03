768b5e61aa77ac6d6469a18db3588d75fd1de86c
s-galvez19 <stikgalvez@hotmail.com>
2018-07-02 21:26:01 -0400

    quiz

---
+++ main.js
@@ -0,0 +1,26 @@
+//Q1
+//A
+var num = 5 + 2;
+
+
+//B
+console.log("hello "+ "my name " + "is " + "Steven " + "i'm " + "doing " + "a test " + "right now " + "about the " + "last class ")
+
+//D
+
+var myArray = [1, 2, 3, 4, "Ana", "Sophia"];
+var Array = ["watch", "phone", "table"];
+
+//e
+var myCar = new Object();
+myCar.make = 'Ford';
+myCar.model = 'Mustang';
+myCar.year = 1969;
+myCar.color = 'black';
+
+var fisthtml = document.Selector()
+var secondhtml = document.()
+var thirdhtml = document.querySelector()
+var fourthhtml = document.querySelector()
+var fivehtml = document.querySelector
+
